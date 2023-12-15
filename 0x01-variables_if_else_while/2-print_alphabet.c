b685f5d2e6449670fc0b74e7016dc7e14b7cf625
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-15 13:24:11 -0800

    A program that writes alphabets in lower case followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- Entry point
+ *
+ * Result: 0 Always (Success)
+ */
+main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= z)
+	{
+		putchar("%c\n", c);
+		c++;
+	}
+}
