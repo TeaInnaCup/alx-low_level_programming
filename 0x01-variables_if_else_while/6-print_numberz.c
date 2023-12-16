758f7a2c19932e6201e90fd7579f364945bcd358
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-15 16:46:38 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	return (0);
+}
