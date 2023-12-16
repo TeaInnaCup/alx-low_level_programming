ae6dee825d34c09a607c6851a55a032a676c01a3
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-15 18:48:15 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,19 @@ int main(void)
 
 	for (num1 = 0; num1 <= 8; num1++)
 	{
-		for (num2 = 1; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
-			if (num2 > num1)
-			{
-				putchar(num1);
-				putchar(num2);
-				if (num1 != 8 && num2 != 9)
+		{
+				putchar('0' + num1);
+				putchar('0' + num2);
+		}
+		{
+				if (num1 != 8 ||  num2 != 9)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}
+		}
 		}
 	}
 	putchar('\n');
