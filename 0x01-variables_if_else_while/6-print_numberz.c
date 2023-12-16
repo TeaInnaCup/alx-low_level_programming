8078d5f0cb4d2d19c600d24d960bbc3793ed634b
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-15 17:11:36 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int number;
 
-	for (number = '0'; number <= 9; number++)
+	for (number = 0; number <= 9; number++)
 	{
-		putchar(number);
+		putchar(number + '0');
 	}
 	putchar('\n');
 	return (0);
