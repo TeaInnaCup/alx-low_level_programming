1421a685c7f399bad5302c90bec712574f98d6eb
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-15 17:46:13 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-    char hexadecimals;
-   
-    for (hexadecimals = 0; hexadecimals <= 9; hexadecimals++)
-    {
-	    putchar(hexadecimals);
-    }
-    for (hexadecimals = 'a'; hexadecimals <= 'f'; hexadecimals++)
-    {
-	    putchar(hexadecimals);
-    }
-    putchar('\n');
-    return (0);
+	char hexadecimals;
+
+	for (hexadecimals = '0'; hexadecimals <= '9'; hexadecimals++);
+	{
+		putchar(hexadecimals);
+	}
+	for (hexadecimals ='a'; hexadecimals <= 'f'; hexadecimals++);
+	{
+		putchar(hexadecimals);
+	}
+	putchar('\n');
+		return (0);
 }
