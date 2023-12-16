da308115154bf1dd8449017c3458ae25f1e0c68b
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-15 16:16:33 -0800

    a program that prints all single digit of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int number;
+
+	number = 0;
+	for (number = 0; number <= 9; number++)
+	{
+		printf("%d\n", number);
+	}
+	return (0);
+}
