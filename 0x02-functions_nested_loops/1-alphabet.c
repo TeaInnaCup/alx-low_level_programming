4225c1fda49368d53d12e073ceb9d83ae33aebc7
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-19 12:33:21 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
-/*
+/**
  * print_alphabet -Entry point
  *
  * Return: void Always (Success)
  */
 void print_alphabet(void)
 {
-	/*
-         * Description: This function prints the lowercase alphabet in a single line.
-         * Return: void (no return value)
+	/**
+	 * Description: prints the lowercase alphabet in a single line.
+         *
+	 * Return: void (no return value)
          */
 	int alphabet;
 
