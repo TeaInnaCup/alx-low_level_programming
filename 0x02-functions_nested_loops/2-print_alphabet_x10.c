2fc9305e27d1e9f96b97edf065bb9337917e7667
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-19 13:04:19 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/** 
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	/** Description - prints alphabets in lowercase 10  times
+	 *
+	 */
+
+int alphabet, increase;
+
+for (increase = 0; increase <= 9; increase++)
+{
+	for (alphabet = 'a'; alphabet <='z'; alphabet++)
+		_putchar(alphabet);
+
+}
+}
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
