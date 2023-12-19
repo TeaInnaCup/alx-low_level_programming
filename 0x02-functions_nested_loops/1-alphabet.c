a2e52af10862af2bce508fdcb69ce106240db3bf
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-19 12:15:49 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /*
- * main - print _putchar
+ * print_alphabet - Entry point
  *
  * Return: 0 Always (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	int alphabet;
 
-	_putchar (alphabet = 'a'; alphabet <= 'z'; alphabet++);
-		_putchar('\n');
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	_putchar(alphabet);	
+	_putchar('\n');
 	return (0);
 }
