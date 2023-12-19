e3d3c4bba4876a755374a103364325e2888fad6b
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2023-12-19 04:41:08 -0800

    o-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
+ * main - print _putchar
  *
  * Return: 0 Always (Success)
  */
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
