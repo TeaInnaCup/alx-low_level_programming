4b70dd832ca53ae9656078200e66be7cd04f396e
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-02 11:54:06 -0800

    1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,12 @@
-/* swap.c */
 #include "main.h"
-
+/**
+ * swap - swaps the value of two integers
+ *
+ * Return: void
+ */
 void swap_int(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
-
