3b4cf0476e96d59e7c937ca8c29a9e812944b42f
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-02 11:39:36 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-void reset_to_98(int *n) {
-    *n = 98;
+void reset_to_98(int *n) 
+/* a function that takes a pointer to an int as parameter and updates the value it points to to 98 */
+{
+	*n = 98;
 }
-
