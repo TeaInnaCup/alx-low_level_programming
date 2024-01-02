c42ab5e93a976d23e7915ce6ba7753ba61d7930a
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-02 12:20:22 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,13 +8,12 @@
  */
 int _strlen(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    while (*s != '\0')
-    {
-        length++;
-        s++;
-    }
-
-    return length;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return length;
 }
