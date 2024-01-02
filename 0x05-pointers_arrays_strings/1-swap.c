17e847eb9c30a808d229f3238aee052b33bf8063
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-02 11:48:05 -0800

    1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,19 +1,10 @@
+/* swap.c */
 #include "main.h"
-#include <stdio.h>
-/** 
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int a;
-	int b;
 
-	a = 98;
-	b = 42;
-	printf("a=%d, b=%d\n", a , b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
+void swap_int(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
+
