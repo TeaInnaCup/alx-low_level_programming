94f0302a0a0bf7fb70466479ac57ab1a06d76677
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-02 15:52:30 -0800

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,26 +11,26 @@ int _atoi(char *s)
     int result = 0;
     int sign = 1;
     int i = 0;
-
+    
     while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-        i++;
-
-    while (s[i] == '-' || s[i] == '+') {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
+	    i++;
+    while (s[i] == '-' || s[i] == '+') 
+    {
+	    if (s[i] == '-')
+		    sign *= -1;
+	    i++;
     }
-
-    while (s[i] >= '0' && s[i] <= '9') {
-        if (result > (2147483647 / 10) || (result == (2147483647 / 10) && s[i] - '0' > 7)) {
-            if (sign == 1)
-                return (2147483647);
-            else
-                return (-2147483648);
-        }
-        result = result * 10 + (s[i] - '0');
-        i++;
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+	    if (result > (2147483647 / 10) || (result == (2147483647 / 10) && s[i] - '0' > 7))
+	    {
+		    if (sign == 1)
+			    return (2147483647);
+		    else
+			    return (-2147483648);
+	    }
+	    result = result * 10 + (s[i] - '0');
+	    i++;
     }
-
     return (result * sign);
 }
