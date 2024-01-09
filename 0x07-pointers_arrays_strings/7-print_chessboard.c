cac8a816853de093d6649d2a0d27121c261a62fe
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-08 18:33:31 -0800

    This function takes a 2D array a representing the chessboard as input and prints its contents row by row.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -27,12 +27,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
+	int i, j;
+
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-			printf("%c", a[i][j]);
+			_putchar(a[i][j]);
 		}
-		printf("\n");
+		_putchar("\n");
 	}
 }
