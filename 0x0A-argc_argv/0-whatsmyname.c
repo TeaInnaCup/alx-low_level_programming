01228df2cc13dd038b0ec27f5bc56a84861992e7
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-12 07:46:30 -0800

    Entry point

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Return: 0 Always (Success)
+ */
+int main(int argc, char *argv[])
+{
+	for(i = 0; arg[0][i] != '\0'; i++)
+	{
+		_putchar(argv[0][i])
+	}
+	_putchar('\n');
+	return (0);
+}
