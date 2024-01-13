f1220215fdf8ca0e5ee836b9edfcdfbd0831ee46
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-13 00:33:28 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,11 @@
 #include "main.h"
 /**
  * main - check code
+ * @argc: argument counter. Counts the number of things entered into the
+ * command line when running the executable on a terminal
+ *
+ * @argv: argument vector. An array that holds the string value of the things
+ * that were entered on the command line when running the program
  *
  * Return: 0 Always (Success)
  */
@@ -9,7 +14,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
