b8af38d2fd725036f93d978a800c946d5918538a
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-13 01:33:19 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-/* main - entry point
- * 
- * @argc: argument count. Counts the number of things entered into the command line when
+/**
+ * main - entry point
+ *
+ * @argc: argument count. Counts the number of things entered into the
+ * command line when
  * running our executable on a terminal
- * 
+ *
  * @argv: argument vector. An array thar holds the string value of the things
  * that were entered on the command line when running the program
  *
