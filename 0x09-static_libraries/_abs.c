99267c059e3e752d5310a4d0c6d8174c7e0b75ee
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-16 10:39:47 -0800

    function declaration of _abs.c

---
+++ 0x09-static_libraries/_abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - calculates the absolute value of a given integer.
+ * @n: the value being checked.
+ *
+ * Return: 0 Always (Success)
+ */
+int _abs(int n) {
+	    return (n < 0 ? -n : n);
+}
