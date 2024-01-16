9cab7843a1e557f0d1eb11671252765fef8647da
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-16 10:22:51 -0800

    a function file to check if a character is a lowercase

---
+++ 0x09-static_libraries/_islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - checks if c is lowercase letter
+ *
+ * @c: character to check
+ * Return: 1 always (success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+}
