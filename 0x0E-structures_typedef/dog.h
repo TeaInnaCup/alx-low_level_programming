0ffeb3cf1e03e3e260fbdd20c40b7e82d4631e36
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-22 03:57:57 -0800

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,3 @@
-/* dog.h */
 #ifndef DOG_H
 #define DOG_H
 /**
@@ -7,10 +6,11 @@
  * @age: The age of the dog
  * @owner: The owner of the dog
  */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
