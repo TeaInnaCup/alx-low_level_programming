841aff4b87881850921f46e8eabf91998fb5c58d
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-22 06:34:32 -0800

    4-main.c

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include "dog.h"
 /**
  * main - check the code
  *
@@ -9,6 +10,10 @@ int main(void)
 	dog_t *my_dog;
 
 	my_dog = new_dog("Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-	return (0);
+	if (my_dog != NULL)
+	{
+		printf("My name is %s, and I am %.1f :)"
+				" - Woof!\n", my_dog->name, my_dog->age);
+	}
+		return (0);
 }
