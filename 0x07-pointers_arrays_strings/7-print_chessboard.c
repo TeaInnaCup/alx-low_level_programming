a153acda286fd90dbdb2fae2264fc062d8d7cf9d
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-24 11:18:18 -0800

    7-print_chessvoard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,40 +1,17 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_chessboard - Prints the contents of a chessboard.
- * @a: The 2D array representing the chessboard.
- *
- * This function prints the chessboard row by row.
- * Each element in the array is a character representing a piece on
- * the board.
- *
- * Example:
- * If a chessboard is represented as:
- *    {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
- *    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
- *    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
- *    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
- *    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
- *    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
- *    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
- *    {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
- * The output of this function will be:
- *    rkbqkbkr
- *    pppppppp
- *
- *    PPPPPPPP
- *    RKBQKBKR
- */
+* print_chessboard - locates a substring
+* @a: array
+*/
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	for (int i = 0; i < 8; i++)
+int i, j;
+for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
 	{
-		for (int j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar("\n");
+	_putchar(a[i][j]);
 	}
+	_putchar('\n');
+}
 }
