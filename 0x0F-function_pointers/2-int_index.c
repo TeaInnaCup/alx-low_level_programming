0aac37567ebc920f4b3cf7a6e93a1c596448fd8c
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-24 10:08:06 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *
+ * @array: array to search in
+ * @size: size of array
+ * @cmp: pointer to the function
+ *
+ * Return: index of the first element for which
+ * the cmp function does not return 0, or -1 if no match is found
+ * or size is negative
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int n;
+
+	if (array && cmp)
+	{
+		for (n = 0; n < size; n++)
+		{
+			if (cmp(array[n]) != 0)
+				return (n);
+		}
+	}
+
+	return (-1);
+}
