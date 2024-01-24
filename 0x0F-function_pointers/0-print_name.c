051c1428c6cf5af581b0def2264072793a6ee90b
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-24 10:38:53 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "function_pointer.h"
+#include "function_pointers.h"
+
 /**
- * print_name - prints a name as is
- * @name: name of the person
- *
- * @f: pointer to function
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to a function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
