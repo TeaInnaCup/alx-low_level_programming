33b59eb3aa2517b7eef0b122bbc27ad2b47987ef
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-27 03:40:29 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,46 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The input string.
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: The converted integer.
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
-    
-    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-	    i++;
-    while (s[i] == '-' || s[i] == '+') 
-    {
-	    if (s[i] == '-')
-		    sign *= -1;
-	    i++;
-    }
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-	    if (result > (2147483647 / 10) || (result == (2147483647 / 10) && s[i] - '0' > 7))
-	    {
-		    if (sign == 1)
-			    return (2147483647);
-		    else
-			    return (-2147483648);
-	    }
-	    result = result * 10 + (s[i] - '0');
-	    i++;
-    }
-    return (result * sign);
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
