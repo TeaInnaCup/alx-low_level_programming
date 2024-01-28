1b56d9c9a0b4556cce318afea975b4b6dd09352e
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-28 02:30:08 -0800

    checks if an integer is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/** _isdigit - checks for digits ( 0 -9 )
+ * @c: integers to be checked
+ * Return: 1 if c is a digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
