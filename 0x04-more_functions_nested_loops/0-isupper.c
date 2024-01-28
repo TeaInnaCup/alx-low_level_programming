60cd5abfbcbac3bbf15336289c1187cda70f140d
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-28 02:15:27 -0800

    checks if a character is aj uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * @c: character to be checked
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
