1927764e875300a103f6aa94c0e0e74ca82df9b9
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-03-25 16:04:36 -0700

    empty function to check if a character is uppercase

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase char
+ * @c: the character to be checked
+ * Return: 1 if character is uppercase, 0 if not
+ */
+int _isupper(int c)
+{
+	return(c >= 'A' && c <= 'Z');
+}
