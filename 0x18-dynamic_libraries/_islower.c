880a53fd0ee5468c6c676db86448e4787d57065d
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-03-25 16:36:36 -0700

    fucntion to check if acharacter is in lowercase

---
+++ 0x18-dynamic_libraries/_islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: character to be checked
+ * Return: 1 on success, 0 if not
+ */
+int _islower(int c)
+{
+	return(c >= 'a' && c <= 'z'):
+}
