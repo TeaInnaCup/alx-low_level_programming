c1fe70d6f558fac9f8ac87b21c7083a13fd0ff5b
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-03-25 16:33:57 -0700

    function to check if an interger is a number that runs betweens 0 - 9

---
+++ 0x18-dynamic_libraries/0-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: integer to be checked
+ *
+ * Return: 1 0n success, 0 if not
+ */
+int _isdigit(int c)
+{
+	return(c >= '0' && c <= '9');
+}
