d57b910b3ac0e1c3c9478203a8c577af4593e52e
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-03-25 17:58:11 -0700

    creating a custom rand()

---
+++ 0x18-dynamic_libraries/override.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+/**
+ * rand - Override for the standard rand() function.
+ *
+ * Return: Always returns the winning number, which is 9.
+ */
+int rand(void) 
+{
+	return 9;
+}
